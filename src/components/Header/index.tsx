bc708fd7ef1e0ac041e7f2b48f7631bb2270bf2f
agelessZeal <35507996+agelessZeal@users.noreply.github.com>
2021-08-16 06:39:10 +0300

    header connect wallet

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -37,6 +37,12 @@ function AppBar(): JSX.Element {
               <div className="flex items-center justify-between">
                 <div className="flex items-center">
                   <Image src="/logo.png" alt="Duly" width="120px" height="32px" />
+                 
+                </div>
+
+                <div className="fixed bottom-0 left-0 z-10 flex flex-row items-center justify-center w-full p-4 lg:w-auto bg-dark-1000 lg:relative lg:p-0 lg:bg-transparent">
+                  <div className="flex items-center justify-between w-full space-x-2 sm:justify-end">
+
                   <div className="hidden sm:block sm:ml-4">
                     <div className="flex space-x-2">
                       {/* <Buy /> */}
@@ -107,10 +113,7 @@ function AppBar(): JSX.Element {
                       )} */}
                     </div>
                   </div>
-                </div>
 
-                <div className="fixed bottom-0 left-0 z-10 flex flex-row items-center justify-center w-full p-4 lg:w-auto bg-dark-1000 lg:relative lg:p-0 lg:bg-transparent">
-                  <div className="flex items-center justify-between w-full space-x-2 sm:justify-end">
                     {/* {chainId && [ChainId.MAINNET].includes(chainId) && library && library.provider.isMetaMask && (
                       <>
                         <QuestionHelper text={i18n._(t`Add xSUSHI to your MetaMask wallet`)}>
@@ -209,7 +212,7 @@ function AppBar(): JSX.Element {
                       </div>
                     )}
 
-                    <div className="w-auto flex items-center rounded bg-dark-900 hover:bg-dark-800 p-0.5 whitespace-nowrap text-sm font-bold cursor-pointer select-none pointer-events-auto">
+                    <div className="w-auto flex items-center rounded-full bg-yellow-900 hover:bg-yellow-800 p-0.5 whitespace-nowrap text-sm font-bold cursor-pointer select-none pointer-events-auto">
                       {account && chainId && userEthBalance && (
                         <>
                           <div className="px-3 py-2 text-primary text-bold">
